ca1afadea6d70400102542bf6b75b0801d6fecae
Victoria Markova <wi16b047@technikum-wien.at>
2018-01-07 17:19:03 +0100

    ...

--- CodingDojo1_Collections/Program.cs
+++ CodingDojo1_Collections/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,37 @@ namespace CodingDojo1_Collections
     {
         static void Main(string[] args)
         {
+            Stack st = new Stack();
+
+            st.Push('A');
+            st.Push('B');
+            st.Push('C');
+            st.Push('D');
+
+            Console.WriteLine("Current stack: ");
+            foreach (char c in st)
+            {
+                Console.WriteLine(c + " ");
+            }
+            //Output: D C B A
+
+            Console.WriteLine();
+            
+            Console.WriteLine("The next poppable value in stack: {0}", st.Peek());
+            
+            Console.WriteLine();
+
+            Console.WriteLine("Removing values: ");
+            st.Pop();
+            st.Pop();
+            
+            Console.WriteLine("Current stack: ");
+            foreach (char c in st)
+            {
+                Console.WriteLine(c + " ");
+            }
+
+            Console.ReadKey();
         }
     }
 }
